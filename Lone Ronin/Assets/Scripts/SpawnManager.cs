071b554aff6d926b97927b2bcae2fb9d251e9363
jjp2767 <jeremiahjpete@gmail.com>
2021-10-21 15:51:50 -0500

    Resized Oni types, corrected spawn locations

--- Lone Ronin/Assets/Scripts/SpawnManager.cs
+++ Lone Ronin/Assets/Scripts/SpawnManager.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] oniPrefabs;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
+    private float spawnRange = 3.5f;
 
     public void Start()
     {
@@ -14,17 +13,13 @@ public class SpawnManager : MonoBehaviour
     }
 
     void SpawnEnemyWave() {
+        Vector3 spawnPos;
+        int oniIndex;
         // Randomly generate oni index and spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-        int oniIndex = Random.Range(0, oniPrefabs.Length);
         for (int i = 0; i < 20; i++) {
+            spawnPos = new Vector3(Random.Range(-spawnRange, spawnRange), 0, Random.Range(-spawnRange, spawnRange));
+            oniIndex = Random.Range(0, oniPrefabs.Length);
             Instantiate(oniPrefabs[oniIndex], spawnPos, oniPrefabs[oniIndex].transform.rotation);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-      
     }
 }
