4409de3be068c26d4f58146706d9a3c97e0f1845
jjp2767 <jeremiahjpete@gmail.com>
2021-11-30 17:12:49 -0600

    Pause Button fixes

--- Lone Ronin/Assets/Scripts/PauseMenu.cs
+++ Lone Ronin/Assets/Scripts/PauseMenu.cs
@@ -6,8 +6,21 @@ public class PauseMenu : MonoBehaviour
     public GameObject pauseScene;
     public static bool isPaused = false;
 
+    // Update is called once per frame
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.P)) {
+            if (isPaused) {
+                Play();
+            }
+            else {
+                Pause();
+            }
+        }
+    }
+
     // function to pause the game
     public void Pause() {
+        isPaused = true;
         pauseScene.SetActive(true);
         Time.timeScale = 0f;
         Debug.Log("Pause Button Pushed");
@@ -17,6 +30,7 @@ public class PauseMenu : MonoBehaviour
     public void Play() {
         pauseScene.SetActive(false);
         Time.timeScale = 1.0f;
+        isPaused = false;
     }
 
     // function to return to main menu
