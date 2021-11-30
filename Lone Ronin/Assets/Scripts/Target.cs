f8a9c56e7913b3d8f7d471d72bbb91d97840caa3
Tnorris706 <taylor.norris@okstate.edu>
2021-11-30 14:11:13 -0600

    Attempt at removing player from parkour after falling

---
+++ Lone Ronin/Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody targetRb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetRb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
