db58907d29bb6189f6848c5bf1190c85509ba696
jjp2767 <jeremiahjpete@gmail.com>
2021-12-01 20:54:36 -0600

    Shadeling update

--- Lone Ronin/Assets/Scripts/EnemyController.cs
+++ Lone Ronin/Assets/Scripts/EnemyController.cs
@@ -4,13 +4,11 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour {
     private Rigidbody enemyRb;
-    //private GameObject player; // for enemies to follow player
     private Animator enemyAnim;
     private AudioSource enemyAudio;
 
     public Transform Player; // for enemies to follow player
     public float movementSpeed;
-    //public float gravityModifier;
     public bool isDead = false;
     public ParticleSystem deathParticle;
     public AudioClip voiceSound;
@@ -22,7 +20,6 @@ public class EnemyController : MonoBehaviour {
         //Physics.gravity *= gravityModifier;
         enemyAnim = GetComponent<Animator>();
         enemyAudio = GetComponent<AudioSource>();
-        //Player = GameObject.Find("Player").transform;
         Player = GameObject.FindWithTag("Player").transform;
     }
 
